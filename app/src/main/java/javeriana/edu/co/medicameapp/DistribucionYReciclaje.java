879f0cacbdb11bdf9c0df7bacac92fcc41580c8a
Betico1928 <betoveluis@gmail.com>
2023-02-17 18:29:04 -0500

    Bindings añadidos a Distribución y Reciclaje

--- app/src/main/java/javeriana/edu/co/medicameapp/DistribucionYReciclaje.java
+++ app/src/main/java/javeriana/edu/co/medicameapp/DistribucionYReciclaje.java
@@ -2,14 +2,22 @@ package javeriana.edu.co.medicameapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+
+import javeriana.edu.co.medicameapp.databinding.ActivityDistribucionYreciclajeBinding;
 
 public class DistribucionYReciclaje extends AppCompatActivity
 {
+    ActivityDistribucionYreciclajeBinding bindingDistribucionYReciclaje;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_distribucion_yreciclaje);
+        bindingDistribucionYReciclaje = ActivityDistribucionYreciclajeBinding.inflate(getLayoutInflater());
+        setContentView(bindingDistribucionYReciclaje.getRoot());
+
+        // Evitar que la pantalla rote
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT); // Para que la app corra en portrait mode
     }
 }
