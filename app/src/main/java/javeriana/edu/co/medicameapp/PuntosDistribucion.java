5d8404fbf626dcaf483a2df145a2da46bda24dde
Kahsma <cajomapa@gmail.com>
2023-02-16 20:38:32 -0500

    Terminada pantalla de mapa

---
+++ app/src/main/java/javeriana/edu/co/medicameapp/PuntosDistribucion.java
@@ -0,0 +1,14 @@
+package javeriana.edu.co.medicameapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PuntosDistribucion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_puntos_distribucion);
+    }
+}
