a961940e33634327b98dad3b7bafaf3fc466b19c
Kahsma <cajomapa@gmail.com>
2023-02-18 13:20:26 -0500

    Bindigs para el controlador de mapas inflados y click liseteners para los botones creados.

--- app/src/main/java/javeriana/edu/co/medicameapp/PuntosDistribucion.java
+++ app/src/main/java/javeriana/edu/co/medicameapp/PuntosDistribucion.java
@@ -3,12 +3,22 @@ package javeriana.edu.co.medicameapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import javeriana.edu.co.medicameapp.databinding.ActivityPuntosDistribucionBinding;
 
 public class PuntosDistribucion extends AppCompatActivity {
-
+    ActivityPuntosDistribucionBinding bindingPuntosDistribucion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_puntos_distribucion);
+        //setContentView(R.layout.activity_puntos_distribucion);
+        bindingPuntosDistribucion = ActivityPuntosDistribucionBinding.inflate(getLayoutInflater());
+        bindingPuntosDistribucion.bButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO implementacion de el boton back para los mapas.
+            }
+        });
     }
 }
