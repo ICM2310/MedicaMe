9e98f460e99df5fd5dba725fc863c1359be09525
Luis Alejandro Bravo Ferreira <luisalejandrobravoferreira@gmail.com>
2023-02-19 01:29:57 -0500

    Pantallas 1,2,3. Inicio, Autenticacion y Registro. "Responsive" (Vertical/Landscape)

---
+++ app/src/main/java/javeriana/edu/co/medicameapp/Registro.java
@@ -0,0 +1,14 @@
+package javeriana.edu.co.medicameapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+    }
+}
