4ebbed7a192950cacf357e5ab3efc269b441adb9
Betico1928 <betoveluis@gmail.com>
2023-02-20 18:48:35 -0500

    Definidos los Intents del Main

--- app/src/main/java/javeriana/edu/co/medicameapp/MainActivity.java
+++ app/src/main/java/javeriana/edu/co/medicameapp/MainActivity.java
@@ -2,13 +2,36 @@ package javeriana.edu.co.medicameapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import javeriana.edu.co.medicameapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity
+{
+    ActivityMainBinding bindingMain;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        bindingMain = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(bindingMain.getRoot());
+
+        bindingMain.BotonUnete.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Log.i("Test", "Se pasa a verificar credenciales");
+
+                // Llamar a la otra interfaz
+                Intent pasarAAutenticacion = new Intent(getBaseContext(), Autenticacion.class);
+                // Arrancarla
+                startActivity(pasarAAutenticacion);
+            }
+        });
     }
 }
