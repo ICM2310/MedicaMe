8aed028db3be360b293522eb5d2bcba57584b725
Betico1928 <betoveluis@gmail.com>
2023-02-27 23:59:56 -0500

    Terminado el Intent al chat desde ReservaExitosa 💫

--- app/src/main/java/javeriana/edu/co/medicameapp/ReservaExitosa.java
+++ app/src/main/java/javeriana/edu/co/medicameapp/ReservaExitosa.java
@@ -2,13 +2,33 @@ package javeriana.edu.co.medicameapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class ReservaExitosa extends AppCompatActivity {
+import javeriana.edu.co.medicameapp.databinding.ActivityReservaExitosaBinding;
+
+public class ReservaExitosa extends AppCompatActivity
+{
+    ActivityReservaExitosaBinding bindingReservaExitosa;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reserva_exitosa);
+        bindingReservaExitosa = ActivityReservaExitosaBinding.inflate(getLayoutInflater());
+        setContentView(bindingReservaExitosa.getRoot());
+
+        bindingReservaExitosa.LinearLayoutMedicamentos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Log.i("ReservaExitosa - onClick", "Flag #1");
+
+                Intent intentIrAlChat = new Intent(getBaseContext(), ChatActivity.class);
+                startActivity(intentIrAlChat);
+            }
+        });
     }
 }
