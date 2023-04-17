64b97c5edeb0fd74d5c9673a658613cd5a447d11
Betico1928 <betoveluis@gmail.com>
2023-04-17 02:12:47 -0500

    Update PuntosDeReciclaje.kt

--- app/src/main/java/javeriana/edu/co/medicameapp/PuntosDeReciclaje.kt
+++ app/src/main/java/javeriana/edu/co/medicameapp/PuntosDeReciclaje.kt
@@ -42,15 +42,6 @@ class PuntosDeReciclaje : AppCompatActivity(), OnMapReadyCallback, OnMarkerClick
         }
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap)
     {
         mMap = googleMap
