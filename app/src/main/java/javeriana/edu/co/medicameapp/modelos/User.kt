5671eddc282706e8d11e94c651f984d839ac5882
Betico1928 <betoveluis@gmail.com>
2023-05-29 17:29:00 -0500

    Dios es grande y misericoridoso :pray:

--- app/src/main/java/javeriana/edu/co/medicameapp/modelos/User.kt
+++ app/src/main/java/javeriana/edu/co/medicameapp/modelos/User.kt
@@ -3,15 +3,17 @@ package javeriana.edu.co.medicameapp.modelos
 class User
 {
     var name : String? = null
-    var email : String? = null
-    var uid : String? = null
+    var eps : String? = null
+    var id : String? = null
 
     constructor(){}
 
-    constructor(name : String?, email : String?, uid: String?)
+    constructor(name: String?, eps: String?, id: String?)
     {
         this.name = name
-        this.email = email
-        this.uid = uid
+        this.eps = eps
+        this.id = id
     }
+
+
 }
