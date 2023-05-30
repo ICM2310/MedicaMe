73c7fa00bb5b8b295d1f9235aa010af4db48ed9c
Betico1928 <betoveluis@gmail.com>
2023-05-30 02:57:57 -0500

    Abrir ChatActivity desde el menú mediante el adaptador :(

--- app/src/main/java/javeriana/edu/co/medicameapp/ChatActivity.kt
+++ app/src/main/java/javeriana/edu/co/medicameapp/ChatActivity.kt
@@ -1,13 +1,25 @@
 package javeriana.edu.co.medicameapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import javeriana.edu.co.medicameapp.databinding.ActivityChatBinding
 
 class ChatActivity : AppCompatActivity()
 {
+    private lateinit var chatBinding: ActivityChatBinding
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_chat)
+        chatBinding = ActivityChatBinding.inflate(layoutInflater)
+        setContentView(chatBinding.root)
+
+        val desdeChatMenu = Intent()
+        val name = desdeChatMenu.getStringExtra("name")
+        val uid = desdeChatMenu.getStringExtra("uid")
+
+        // supportActionBar?.title = name
+
     }
 }
--- app/src/main/java/javeriana/edu/co/medicameapp/adapters/UserAdapter.kt
+++ app/src/main/java/javeriana/edu/co/medicameapp/adapters/UserAdapter.kt
@@ -1,11 +1,14 @@
 package javeriana.edu.co.medicameapp.adapters
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import javeriana.edu.co.medicameapp.ChatActivity
 import javeriana.edu.co.medicameapp.R
 import javeriana.edu.co.medicameapp.modelos.User
 
@@ -29,6 +32,16 @@ class UserAdapter(val context : Context, val userList : ArrayList<User>) :
         val currentUser = userList[position]
 
         holder.textName.text = currentUser.name
+
+        holder.itemView.setOnClickListener {
+
+            val pasarAChatActivity = Intent(context, ChatActivity::class.java)
+
+            pasarAChatActivity.putExtra("name", currentUser.name)
+            pasarAChatActivity.putExtra("uid", FirebaseAuth.getInstance().currentUser?.uid)
+
+            context.startActivity(pasarAChatActivity)
+        }
     }
 
     class UserViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView)
