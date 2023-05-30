660659f6ca121a6f500b09f830337c165d4e8436
Betico1928 <betoveluis@gmail.com>
2023-05-30 01:23:45 -0500

    Creado nueva actividad de chat

---
+++ app/src/main/java/javeriana/edu/co/medicameapp/ChatActivity.kt
@@ -0,0 +1,13 @@
+package javeriana.edu.co.medicameapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChatActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
